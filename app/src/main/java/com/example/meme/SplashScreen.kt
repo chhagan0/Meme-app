858ab8563a688d1eb214c4aa6cf5515aaa8e14a8
chhagan <chhaganswami1@gmail.com>
2022-11-10 12:10:16 +0530

    meme

---
+++ app/src/main/java/com/example/meme/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.example.meme
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        var splashScreen = findViewById<ImageView>(R.id.memeOn)
+        splashScreen.alpha= 0f
+        splashScreen.animate().setDuration(1500).alpha(1f).withEndAction {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
